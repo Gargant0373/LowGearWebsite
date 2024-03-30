0d33b8f09fa96913c06ddfa937f7525c59eec6e6
Gargant <gargant0373@gmail.com>
2024-03-30 18:56:20 +0100

    Updated c0ontent

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ function scrollToElement(elementId) {
         top: element.offsetTop - 100
     });
 }
+
+function switchElements(elementId1, elementId2) {
+    var element1 = document.getElementById(elementId1);
+    var element2 = document.getElementById(elementId2);
+    if (!element1 || !element2) return;
+
+    var parent = element1.parentElement;
+    if (!parent) return;
+
+    parent.removeChild(element2);
+    parent.insertBefore(element2, element1);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if(window.innerWidth > 768) return;
+    switchElements('s0_1', 's0_2');
+});
